70cae1b63aea0b7adf5d2c07da356cd663f621b8
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2020-11-19 12:11:11 +0200

    Add user model and create form old fashioned way

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum: 8 }, uniqueness: true
+  validates :email, presence: true,
+                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'Email invalid' },
+                    uniqueness: { case_sensitive: false }, length: { in: 3..50 }
+  validates :password, presence: true, length: { minimum: 8 }
+end
